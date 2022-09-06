5bf884945e60666eb65364bf1ba3367b021b8ad8
PaulSeitzISU <97689370+PaulSeitzISU@users.noreply.github.com>
2022-09-05 21:56:06 -0500

    figure commit

---
+++ Unit1/figure/sketch.js
@@ -0,0 +1,175 @@
+function setup() {
+  // Sets the screen to be 720 pixels wide and 400 pixels high
+  createCanvas(720, 600);
+  ellipseMode(CENTER);
+  noStroke();
+
+  // triangle(18, 18, 18, 360, 81, 360);
+  // rect(81, 81, 63, 63);
+  // quad(189, 18, 216, 18, 216, 360, 144, 360);
+  // ellipse(252, 144, 72, 72);
+  // triangle(288, 18, 351, 360, 288, 360);
+}
+
+function draw() {
+  background(100);
+
+  if (mouseIsPressed) {
+  background("lightblue");
+      fill("#ECD5B5");
+    triangle(548 , 222 ,  
+635 , 222 ,  
+594 , 279 );
+      fill("#ECD5B5");
+    triangle(550 , 323 ,  
+591 , 262 ,  
+634 , 322);
+    
+    
+     fill("tan");
+    rect(543 , 315 , 100, 20);
+     fill("tan");
+    rect(543 , 215 , 100, 20);
+    
+    
+    
+    fill("tan");
+    rect(258, 176, 20, 50);
+    fill("rgb(202,18,202)");
+    rect(220, 208, 100, 700);
+    fill("rgb(223,99,223)");
+    triangle(244, 209, 271, 243, 299, 208);
+    fill("rgb(255,255,255)");
+    triangle(248, 209, 270, 236, 296, 207);
+    fill("rgb(104,17,104)");
+    triangle(261, 210, 268, 219, 277, 206);
+    fill("rgb(104,17,104)");
+    triangle(267, 213, 262, 227, 279, 227);
+    fill("rgb(104,17,104)");
+    triangle(270, 236, 262, 227, 279, 227);
+    fill("rgb(104,17,104)");
+    triangle(272, 244, 275, 399, 281, 400);
+    fill("rgb(202,18,202)");
+
+    quad(219, 207, 231, 230, 150, 303, 137, 277);
+    fill("rgb(202,18,202)");
+
+    quad(318, 210, 390, 270, 371, 288, 306, 235);
+    fill("rgb(202,18,202)");
+
+    quad(169 , 285 ,  
+146 , 300 ,  
+106 , 229 ,  
+135 , 218);
+    fill("rgb(202,18,202)");
+
+    quad(362 , 281 ,  
+380 , 293 ,  
+436 , 211 ,  
+410 , 199);
+    fill("rgb(104,17,104)");
+    triangle(219, 242, 217, 237, 177, 278);
+
+    fill("rgb(104,17,104)");
+    triangle(321, 249, 322, 242, 352, 271);
+
+    fill("yellow");
+    triangle(274, 89, 244, 184, 187, 185);
+    fill("#FFD75F");
+    triangle(240, 185, 347, 182, 271, 90);
+
+    fill("rgb(104,17,104)");
+    quad(183, 182, 242, 182, 242, 187, 185, 187);
+
+    fill("rgb(104,17,104)");
+    quad(238, 183, 347, 179, 347, 183, 243, 188);
+
+    fill("rgb(104,17,104)");
+    quad(182, 182, 272, 85, 274, 92, 189, 182);
+
+    fill("rgb(104,17,104)");
+    quad(269, 91, 342, 181, 348, 179, 273, 88);
+    fill("rgb(104,17,104)");
+
+    ellipse(287, 153, 50, 32);
+
+    fill("rgb(0,0,0)");
+
+    ellipse(287, 153, 45, 27);
+    fill("rgb(255,255,255)");
+
+    ellipse(293, 150, 15, 15);
+  } else {
+    fill("tan");
+    rect(258, 176, 20, 50);
+    fill("rgb(202,18,202)");
+    rect(220, 208, 100, 700);
+    fill("rgb(223,99,223)");
+    triangle(244, 209, 271, 243, 299, 208);
+    fill("rgb(255,255,255)");
+    triangle(248, 209, 270, 236, 296, 207);
+    fill("rgb(104,17,104)");
+    triangle(261, 210, 268, 219, 277, 206);
+    fill("rgb(104,17,104)");
+    triangle(267, 213, 262, 227, 279, 227);
+    fill("rgb(104,17,104)");
+    triangle(270, 236, 262, 227, 279, 227);
+    fill("rgb(104,17,104)");
+    triangle(272, 244, 275, 399, 281, 400);
+    fill("rgb(202,18,202)");
+
+    quad(219, 207, 231, 230, 150, 303, 137, 277);
+    fill("rgb(202,18,202)");
+
+    quad(318, 210, 390, 270, 371, 288, 306, 235);
+    fill("rgb(202,18,202)");
+
+    quad(157, 261, 189, 325, 169, 341, 133, 277);
+    fill("rgb(202,18,202)");
+
+    quad(396, 274, 351, 336, 335, 314, 378, 263);
+    fill("rgb(104,17,104)");
+    triangle(219, 242, 217, 237, 177, 278);
+
+    fill("rgb(104,17,104)");
+    triangle(321, 249, 322, 242, 352, 271);
+
+    fill("yellow");
+    triangle(274, 89, 244, 184, 187, 185);
+    fill("#FFD75F");
+    triangle(240, 185, 347, 182, 271, 90);
+
+    fill("rgb(104,17,104)");
+    quad(183, 182, 242, 182, 242, 187, 185, 187);
+
+    fill("rgb(104,17,104)");
+    quad(238, 183, 347, 179, 347, 183, 243, 188);
+
+    fill("rgb(104,17,104)");
+    quad(182, 182, 272, 85, 274, 92, 189, 182);
+
+    fill("rgb(104,17,104)");
+    quad(269, 91, 342, 181, 348, 179, 273, 88);
+    fill("rgb(104,17,104)");
+
+    ellipse(287, 153, 50, 32);
+
+    fill("rgb(0,0,0)");
+
+    ellipse(287, 153, 45, 27);
+    fill("rgb(255,255,255)");
+
+    ellipse(293, 150, 15, 15);
+  }
+  fill("rgb(0,0,0)")
+  rect(0, 400, 720, 200)
+  fill("white")
+  rect(10, 410, 700, 180)
+  textSize(18);
+  fill("rgb(0,0,0)")
+  text('This is Government, he protects the world through time traveling and stopping crimes before they happen. He is also behind all governments around the world. He is the man upstairs. He is Government. He does this in the shadows using time travel. He thinks the right thing to do is to manipulate world governments so they can arrest more criminals.',20,420,680,160)
+}
+
+function mouseReleased() {
+  print(mouseX + " , " + mouseY + " , ");
+}
