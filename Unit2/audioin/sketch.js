5b067666ccaada32ec2109be2b63ef27b21b22e7
PaulSeitzISU <97689370+PaulSeitzISU@users.noreply.github.com>
2022-10-10 20:15:48 -0500

    h

--- Unit2/audioin/sketch.js
+++ Unit2/audioin/sketch.js
@@ -7,16 +7,27 @@ function setup() {
   // code for initializing mic in.
   mic = new p5.AudioIn(); // what does "new" mean?
   mic.start();
+  fft = new p5.FFT();
+  fft.setInput(mic);
 }
 
 function draw() {
-  background("green");
+  background("yellow");
 
+  let spectrum = fft.analyze();
+
+  fill("yellow");
+  beginShape();
+  for(i = 0; i < spectrum.length; i++){
+    vertex(i, map(spectrum[i], 0, 255, height, 0))
+  }
+  endShape();
   // get the sound input
   vol = mic.getLevel(); // returned level is between 0 and 1
 
   // text on the screen for debugging
   textSize(18);
+  fill("black")
   text(
     "Click the screen first to give\npermission for mic input.\nMy volume is " +
       vol.toFixed(3),
@@ -28,6 +39,8 @@ function draw() {
   //  x = vol*200 ;
   x = map(vol, 0, 1, 0, width);
   rect(x, 200, 50, 50);
+
+
 }
 
 // you need this code for audio programs and also, the user
