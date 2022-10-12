9c3065ffbb3a79b4b2a5e6cbd84f2b4f3c9741f6
PaulSeitzISU <97689370+PaulSeitzISU@users.noreply.github.com>
2022-10-12 15:51:35 -0500

    f

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -1,6 +1,9 @@
-let state = 2;
+let state = 0;
 let timer = 3;
+let x = 0;
 let i1,i2,i3;
+const letters = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`
+
 //eye stuff
 let capture = null;
 let tracker = null;
@@ -11,8 +14,15 @@ let centerEye
 
 let mic, fft;
 
+let letterArry = [];
+
 
 function setup() {
+
+  for(let i = 0; i  < 200; i++){
+    letterArry.push(new Letter());
+  }
+
   // code for initializing mic in.
   mic = new p5.AudioIn(); // what does "new" mean?
   mic.start();
@@ -41,11 +51,10 @@ function setup() {
   mic.start();
   fft = new p5.FFT();
   fft.setInput(mic);
-
 //image loading
 
-  // i1 = loadImage("assets/Card.jpg");
-  // i2 = loadImage("assets/What.jpg");
+   i1 = loadImage("assets/HYPNO.png");
+   i2 = loadImage("assets/blue.jpg");
   // i3 = loadImage("assets/Doge.jpg");
 
   imageMode(CENTER); 
@@ -54,7 +63,6 @@ function setup() {
 }
 
 function draw() {
-background(220);
 textSize(24);
 
 
@@ -62,16 +70,21 @@ textSize(24);
 
 switch(state){
   case 0:
+    background(220);
+
     fill("pink")
 rect (width/2,height/2,200,200)
-
+fill("black")
+text("Yes!!",width/2,height/2)
     fill("black")
     text("Are You There?", width/2 , height/8)
 
  
   break;
   case 1:
-    eyeTrack();
+    background(220);
+
+  eyeTrack();
 
 
 
@@ -82,6 +95,7 @@ rect (width/2,height/2,200,200)
       text(nfc(timer,1),width/2 , height/3)
       if(timer < 0){
         state++;
+        timer = 3;
       }
     }else {
       fill("black")
@@ -92,25 +106,111 @@ rect (width/2,height/2,200,200)
   break;
 
   case 2:
-    spectrumLoad();
+    background(220);
 
+
+  fill("black")
+    text("Gathering Audio Sample", width/2 , height/8)
+    spectrumLoad();
+    timer -= .1
+    if(timer < 0){
+      state++;
+      timer = 3;
+    }
 
 
      break;
   case 3: 
 
+for(let i = 0; i  < letterArry.length; i++){
+  letterArry[i].display();
+  letterArry[i].move();
+}
+
+
+    // for(let j = 0;j < 100;j++){
+    //   for(let i = 100;i > 0;i--){
+    //     const rand = int(random(0, letters.length-1));
+    //       fill('green');
+    //       text(letters[rand],i*40,j*40)
+    //     }}
+
+        timer -= .1
+        if(timer < 0){
+          state++;
+          timer = 5;
+        }
 
   break;
+case 4:
+  if (mouseX > 0 && mouseX < windowWidth && mouseY > 0 && mouseY < windowHeight) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+  }
+  image(i2,width/2,height/2,windowWidth,windowHeight)    
+  timer -= .1
 
+  if(timer < 0){
+      state = 0;
+      timer = 3;
+
+    }
+break;
 
 }
 
 }
+
+
+class Letter {
+
+  // constructor and attributes
+  constructor() {
+    this.pos = createVector(random(width) * 30,random(height)* 10) ;  // initialize your attributes here
+    this.vel = createVector(0,random(6,1))
+    this.color = color(0,random(40,200),0)
+    this.lran = int(random(0, letters.length-1));
+    this.scale = random(12,60)
+
+
+  }
+
+  // methods
+
+  display() {
+    fill("black");
+    textSize(this.scale + 5)
+    text(letters[this.lran],this.pos.x,this.pos.y)
+    fill(this.color);
+    textSize(this.scale)
+    text(letters[this.lran],this.pos.x,this.pos.y)
+
+  }
+
+  move() {
+    this.pos.add(this.vel);
+    if(this.pos.x > width){
+      this.pos.x = -10;
+    }
+    if(this.pos.x < -10){
+      this.pos.x = width;
+    }
+    if(this.pos.y > height){
+      this.pos.y = -10;
+    }
+    if(this.pos.y < -10){
+      this.pos.y = height;
+    }
+  }
+  
+}
+
 
 function spectrumLoad(){
   stroke(51);
+  noFill();
   let spectrum = fft.analyze();
-  noFill()
+
   beginShape();
   for (i = 0; i < spectrum.length; i++) {
     vertex(i, map(spectrum[i], 0, 255, height, 0));
@@ -155,19 +255,14 @@ function getPoint(index) {
 }
 
 function drawEye(eye1, eye2) {
+  image(i1,width/2,height/2, 200, 200);
+ 
+
   noFill();
   stroke(255, 0.4);
-  fill("black")
+  fill("red")
   centerEye = createVector((eye2.center.x - eye1.center.x)/2 + eye1.center.x,(eye2.center.y - eye1.center.y)/2 + eye1.center.y )
   text("X",centerEye.x,centerEye.y)
-  testVeiwing(centerEye);
-}
-
-function testVeiwing(centerEye){
-  
-  fill("yellow")
-  rect(width/2,height/2,50,50)
-
 
 }
 
